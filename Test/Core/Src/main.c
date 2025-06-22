be3d577159e1ece635289f0f190f4888cdfccfc0
Saranga Senarathna <saranganirwana@gmail.com>
2025-06-22 16:41:27 +0530

    change qty

--- Test/Core/Src/main.c
+++ Test/Core/Src/main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-//test1
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
